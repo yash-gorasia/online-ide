8e6ed2dbe1b42c971820268f64d8174705f714c3
Yash Gorasia <yash.gorasia118198@marwadiuniversity.ac.in>
2024-11-16 10:46:25 +0530

    made component of themdropdown and listed all the themes for editor in defineTheme.js

---
+++ frontend/src/components/ThemeDropdown.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Select from 'react-select'
+import monacoThemes from 'monaco-themes/themes/themelist'
+import { customStyles } from '../constants/customStyles'
+
+
+const ThemeDropdown = ({ handleThemeChange, theme }) => {
+    return (
+        <div>
+            <Select
+                placeholder={`Select Theme`}
+                options={Object.entries(monacoThemes).map(([themeId, themeName]) => ({
+                    label: themeName,
+                    value: themeId,
+                    key: themeId
+                }))}
+                value={theme}
+                styles={customStyles}
+                onChange={(selectedOption) => handleThemeChange(selectedOption)}
+            />
+        </div>
+    )
+}
+
+export default ThemeDropdown
